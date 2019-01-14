8199418a45c5510e4fb9054611ebe6d88d52024c
Nathan Kern - External <nathan.kern@centene.com>
2019-01-13 23:15:56 -0600

    minor improvements to test bed

--- test-bed/src/app/app-routing.module.ts
+++ test-bed/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'tests', loadChildren: './tests/tests.module#TestsModule'},
+  // { path: 'tests', loadChildren: './tests/tests.module#TestsModule'},
 ];
 
 @NgModule({
--- test-bed/src/app/app.component.ts
+++ test-bed/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, NgZone } from '@angular/core';
 import { TestsQuery, TestsService } from './tests/state';
-import { akitaDevtools } from '@datorama/akita/src/enhancers/devtools';
+import { akitaDevtools } from '@datorama/akita';
 import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent {
   title = 'the test bed';
-  // tests$ = this.testsQuery.tests$;
+
   constructor(private ngZone: NgZone) {
     if (!environment.production) {
       akitaDevtools(ngZone);
